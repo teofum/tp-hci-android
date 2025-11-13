7c70d567c4b9493b66825b4427985c9f6eb3be47
Sebalee <slee@itba.edu.ar>
2025-11-13 17:41:26 -0300

    feat!: screens modularization

--- app/src/main/java/com/example/tphci/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/tphci/ui/profile/ProfileScreen.kt
@@ -4,10 +4,64 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tphci.MyApplication
+import com.example.tphci.ui.home.HomeViewModel
 
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(
+    viewModel: HomeViewModel = viewModel(
+        factory = HomeViewModel.provideFactory(
+            (LocalContext.current.applicationContext as MyApplication).sessionManager,
+            (LocalContext.current.applicationContext as MyApplication).userRepository,
+            (LocalContext.current.applicationContext as MyApplication).shoppingRepository
+        )
+    )
+) {
 
-    Text("Profile TODO", style = MaterialTheme.typography.headlineMedium)
+    val uiState = viewModel.uiState
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("Perfil", style = MaterialTheme.typography.headlineMedium)
+
+
+
+        if (!uiState.isAuthenticated) {
+            Text("Debug login")
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {
+                // TODO hardcodeado (hacerlo con form)
+                // pueden ir a http://localhost:8080/docs con la API en Try it out
+                // después ejecutar desde la página:
+                // {
+                //  "name": "test",
+                //  "surname": "test",
+                //  "email": "test@gmail.com",
+                //  "password": "test1234",
+                //  "metadata": {}
+                // }
+                // y después verificarlo
+                viewModel.login("test@gmail.com", "test1234")
+            }) {
+                Text("hardcode login")
+            }
+        } else {
+
+
+            Text("User login ok")
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(onClick = { viewModel.logout() }) {
+                Text("Cerrar sesión")
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/example/tphci/ui/shopping_list/ShoppingListScreen.kt
+++ app/src/main/java/com/example/tphci/ui/shopping_list/ShoppingListScreen.kt
@@ -4,11 +4,30 @@ package com.example.tphci.ui.shopping_list
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tphci.MyApplication
+import com.example.tphci.ui.home.HomeViewModel
 
 @Composable
-fun ShoppingListScreen() {
+fun ShoppingListScreen(
+    viewModel: HomeViewModel = viewModel(
+        factory = HomeViewModel.provideFactory(
+            (LocalContext.current.applicationContext as MyApplication).sessionManager,
+            (LocalContext.current.applicationContext as MyApplication).userRepository,
+            (LocalContext.current.applicationContext as MyApplication).shoppingRepository
+        )
+    )
+) {
+
+    val uiState = viewModel.uiState
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -18,7 +37,67 @@ fun ShoppingListScreen() {
 
 
 
+        // agregar lista
+        val listName = remember { mutableStateOf("") }
+        val listDescription = remember { mutableStateOf("") }
+        val recurring = remember { mutableStateOf(false) }
 
+
+        OutlinedTextField(
+            value = listName.value,
+            onValueChange = { listName.value = it },
+            label = { Text("Nombre de la lista") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = listDescription.value,
+            onValueChange = { listDescription.value = it },
+            label = { Text("Descriptión de la lista") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Recurrente")
+            Switch(
+                checked = recurring.value,
+                onCheckedChange = { recurring.value = it },
+                colors = SwitchDefaults.colors(checkedThumbColor = Color.Green)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {
+            viewModel.addShoppingList(
+                name = listName.value,
+                description = listDescription.value,
+                recurring = recurring.value
+            )
+        }) {
+            Text("Agregar lista")
+        }
+
+
+        // listas
+        Button(onClick = { viewModel.getShoppingLists() }) {
+            Text("Obtener listas")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Listas empty: ${uiState.shoppingLists.isEmpty()}")
+
+        Text("Listas:")
+
+        uiState.shoppingLists.forEach { list ->
+            Text("- ${list.name}")
+        }
 
 
     }
