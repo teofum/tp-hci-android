99ee4db9d21be84fc523e13c27265e16d6dbabd3
lolo <lpizzutobeltran@gmail.com>
2025-11-13 14:16:43 -0300

    fixed merge shit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+
     // Retrofit + Gson converter
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
