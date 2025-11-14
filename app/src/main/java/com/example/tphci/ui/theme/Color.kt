cc2652f57ba0027dcf71320c7e90875a35965b35
lolo <lpizzutobeltran@gmail.com>
2025-11-14 10:28:40 -0300

    stuff

--- app/src/main/java/com/example/tphci/ui/theme/Color.kt
+++ app/src/main/java/com/example/tphci/ui/theme/Color.kt
@@ -3,9 +3,14 @@ package com.example.tphci.ui.theme
 import androidx.compose.ui.graphics.Color
 
 /// Note: these are ARGB
-val darkGreen  = Color(0xFF223030)
-val darkGreen2 = Color(0xBF223030)
+val darkGreen   = Color(0xFF223030)
+val darkGreen75 = Color(0xBF223030)
+val darkGreen20 = Color(0x33223030)
 
 val offWhite = Color(0xFFECE7DF)
 
 val offWhite2 = Color(0xFFD8D3CD)
+
+val deleteRed  = Color(0xFFEF3F3F)
+
+val deleteRed2 = Color(0x33EF3F3F)
